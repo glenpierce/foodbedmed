53920410f481feecb92d5385f71248883c974348
Hardeep Shoker <shokerhardeep@gmail.com>
2016-03-05 19:32:34 -0500

    adding google service

--- server.js
+++ server.js
@@ -2,15 +2,22 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var xmlParser = require('express-xml-bodyparser');
+var request = require('request');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({    extended: true    }));
 app.use(xmlParser());
 
-app.post('/twilio', function(req, res) {
+    var uri = "https://maps.googleapis.com/maps/api/geocode/json?address=" + 
+         ""
+         + "&key=";
+    request(uri, function(err, resp) {
+        console.log(resp.body);
+    });
+/*app.post('/twilio', function(req, res) {
     res.sendStatus(200);
 });
 
 app.listen(process.env.PORT || 3000, function(data) {
     console.log('server running.');
-});
+});*/
