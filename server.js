c3c4f1cd48f62beb4c2d678d8d91de23fc1604fe
Hardeep Shoker <shokerhardeep@gmail.com>
2016-03-05 19:07:39 -0500

    Adding parsing

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var xmlParser = require('express-xml-bodyparser');
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({    extended: true    }));
+app.use(xmlParser());
 
-app.post('/twillo', function(req, res) {
-    console.log(req);
+app.post('/twilio', function(req, res) {
     res.sendStatus(200);
 });
 
 app.listen(process.env.PORT || 3000, function(data) {
-    console.log(data);
+    console.log('server running.');
 });
